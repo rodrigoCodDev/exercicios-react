46b78946aaef179a4ef5880d7149960ef7bfaf5c
Rodrigo <rodrigo.oliveira200310@gmail.com>
2025-01-26 11:21:07 -0300

    Primeiro componente

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Primeiro = () => {
+    return <>
+        <h2>Meu primeiro componente!</h2>
+    </>
+}
+
+export default Primeiro
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Primeiro from './components/Primeiro';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Renderização - Código JSX
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    <Primeiro />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
